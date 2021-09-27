7f0d6c71d0bf186c3290edf0b9ed171a421f2690
nadyaananda <nadyaanandasuryana06@gamail.com>
2021-09-27 23:24:06 +0700

    Tugas WP

---
+++ application/controllers/latihan.php
@@ -0,0 +1,49 @@
+<?php
+class latihan extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>BIODATA</h1>";
+        echo "&nbsp;DATA DIRI
+        <br>
+        <br>
+        <table width='700' height='200' border='0' >
+        <tr>
+        <td width='50'>&nbsp;</td>
+        <td width='200' height='5'>NAMA LENGKAP</td>
+        <td>:</td>
+        <td width='500'> NADYA ANANDA SURYANA</td>
+        </tr>
+        <tr>
+        <td>&nbsp;</td>
+        <td>NAMA PANGGILAN</td>
+        <td>:</td>
+        <td>NADYA</td>
+        </tr>
+        <tr>
+        <td>&nbsp;</td>
+        <td> TTL</td>
+        <td> :</td>
+        <td>JAKARTA, 06 MEI 2001</td>
+        </tr>
+        <tr>
+        <td>&nbsp;</td>
+        <td> Alamat</td>
+        <td>:</td>
+        <td>TAMBUN, KAB.BEKASI</td>
+        </tr>
+        <tr>
+        <td>&nbsp;</td>
+        <td> Agama</td>
+        <td>:</td>
+        <td>ISLAM</td>
+        </tr>
+        <tr>
+        <td height='21'>&nbsp;</td>
+        <td> Hobi</td>
+        <td>:</td>
+        <td>Mancing</td>
+        </tr>
+        </table>";
+    }
+}
